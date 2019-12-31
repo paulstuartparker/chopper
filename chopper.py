a15a5b428cd15fd1bbac4cc1572f89b33ffc94d9
Paul Parker <paul.parker@blinkhealth.com>
2019-12-31 15:48:43 -0500

    config

--- chopper.py
+++ chopper.py
@@ -4,18 +4,19 @@ import librosa
 import soundfile as sf
 import numpy as np
 
-# detect the current working directory and print it
 apath = "./assets/"
 newpath = "./new/samples"
-hop = 882
+hop = 2048
 sr = 44100
+os.system("rm -rf new")
+os.system("mkdir new")
 
 
-def process_samples(track, fname, sr=44100, hop=2205):
+def process_samples(track, fname, sr=44100, hop=2048):
 
     oenv = librosa.onset.onset_strength(y=track, sr=sr, hop_length=hop)
 
-    onsets = librosa.onset.onset_detect(y=track, onset_envelope=oenv, backtrack=False)
+    onsets = librosa.onset.onset_detect(onset_envelope=oenv, backtrack=False)
 
     onset_bt = librosa.onset.onset_backtrack(onsets, oenv)
 
@@ -23,19 +24,27 @@ def process_samples(track, fname, sr=44100, hop=2205):
     sfbt = librosa.frames_to_samples(onset_bt, hop_length=hop)
 
     for i, s in enumerate(sfbt):
-        print(s)
-        print("ssss")
+
         diff = sft[i] - s
-        print(diff)
-        print("DIFFFF")
-        end = sft[i] + diff + hop
-        start = s - hop
+        if i + 1 < len(sft):
+            end = sft[i + 1] - 512
+        else:
+            end = sft[i] + diff
+        start = s - 512
+
         if start < 0:
             start = 0
         if end > len(track):
             print("DONE")
             break
         new = track[start:end]
+
+        print(diff)
+        print("difff")
+        print(start)
+        print("start")
+        print(end)
+        print("end")
         newname = fname + str(i) + ".wav"
         print(new)
         print(newname)
@@ -56,11 +65,11 @@ for file in os.listdir(folder):
 
 for fp in files:
     fp = str(fp)
-    track, sr = librosa.load(fp, sr=sr, offset=0.5, duration=None, mono=True)
+    track, sr = librosa.load(fp, sr=sr, offset=0.5, duration=15, mono=True)
 
     low = librosa.effects.percussive(track)
-    high = librosa.effects.harmonic(track)
-
+    # high = librosa.effects.harmonic(track)
+    #
     process_samples(low, newpath + "perc_")
     # process_samples(high, newpath + "harm_")
     # process_samples(track, newpath + "def_")
